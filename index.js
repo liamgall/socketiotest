1594c5523615188e4c5c186ec2c648003537a708
liamgall <liamgall@naver.com>
2016-12-05 01:17:14 +0900

    login out

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var players = 0;
+var userPool = new Array();
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
-
-io.sockets.emit('an event sent to all connected clients');
-
 io.on('connection', function(socket){
-    var player = ++players;
-    console.log(players + "connected");
-    socket.on('chat', function(msg){
-        io.emit('chat message', 'user'+player +' : ' + msg);
+    socket.emit('to_client', {msg:"hello"});
+    socket.broadcast.emit('to_client', {msg:"user login."});
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('to_client',{msg:"user logout."});
+    })
+    socket.on('from_client', function(data){
+        console.log(data.msg);
+        io.sockets.emit('to_client',{ msg:data.msg });
     });
 });
-
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
